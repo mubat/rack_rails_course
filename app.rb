a1606271cd50d21104228ae385702f9fe241f61e
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-27 01:17:17 +0300

    create Rack app to show time in requested format

--- app.rb
+++ app.rb
@@ -1,13 +1,33 @@
+# frozen_string_literal: true
+
+require 'cgi'
+
 class App
+  SUPPORT_FORMATS = {
+    'year' => '%Y',
+    'month' => '%m',
+    'day' => '%d',
+    'hour' => '%H',
+    'minute' => '%M',
+    'second' => '%S'
+  }.freeze
 
   def call(env)
-    [status, headers, body]
+    @env = env
+    parse
+    validate_formats
+    @status = status
+    @headers = headers
+    @body = body
+    [@status, @headers, @body]
   end
 
   private
 
   def status
-    200
+    return 400 if errors?
+
+    @env['REQUEST_PATH'] == '/time' ? 200 : 404
   end
 
   def headers
@@ -15,6 +35,28 @@ class App
   end
 
   def body
-    ["Welcome aboard!\n"]
+    return ["Unknown time format [#{@not_support_formats.join(', ')}]\n"] if errors?
+    return [] unless @status == 200
+
+    ["#{Time.now.strftime(format)}\n"]
+  end
+
+  def parse
+    @params = CGI.parse(@env['QUERY_STRING'])
+    @request_format = @params['format'][0]
+  end
+
+  def validate_formats
+    @not_support_formats = @request_format.split(',') - SUPPORT_FORMATS.keys
+  end
+
+  def errors?
+    !@not_support_formats.empty?
+  end
+
+  def format
+    format = @request_format.split(',')
+    format.map! { |t| t = SUPPORT_FORMATS[t] }
+    format.join('-')
   end
 end
