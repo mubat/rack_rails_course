736e5ba9c197b7b63310434649d1ac2258ced626
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-27 23:28:33 +0300

    refactor app's main class to make on common method that handle request

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'cgi'
+require 'pry'
 require_relative 'handler_time_format'
 
 class App
@@ -10,33 +11,38 @@ class App
 
   def call(env)
     @request = Rack::Request.new(env)
-    process
-    response = Rack::Response.new(body, status, headers)
-    response.finish
+    initialize_response_arguments
+    process_call
+    Rack::Response.new(@body, @status, @headers).finish
   end
 
   private
 
-  def process
-    return unless @handlers.key?(@request.path.to_sym)
+  ##
+  # main method of incoming call handling
+  # It Prepare 3 states:
+  #   1 - if incoming path not support (404)
+  #   2 - if handler has errors (400 and errors message in body)
+  #   3 - normal state when request handled successfully
+  #  All addition logic will be described inside each handler according it business logic
+  def process_call
+    unless @handlers.key?(@request.path.to_sym)
+      @status = 404
+      return
+    end
 
-    @handler = @handlers[@request.path.to_sym].new(@request.params['format'])
+    handler = @handlers[@request.path.to_sym].new(@request.params['format'])
+    @status = 400 if handler.errors?
+    @body = handler.errors? ? handler.errors : handler.prepare_result
   end
 
-  def status
-    return 404 unless @handlers.key?(@request.path.to_sym)
-
-    @handler.errors? ? 400 : 200
+  ##
+  # Initialization executes in separate action
+  # It's need to clean data for each request call
+  def initialize_response_arguments
+    @body = nil
+    @status = 200
+    @headers = { 'Content-Type' => 'text/plain' }
   end
 
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-
-  def body
-    return unless @handlers.key?(@request.path.to_sym)
-    return @handler.errors if @handler.errors?
-
-    @handler.handle
-  end
 end
