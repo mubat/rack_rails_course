2e355e8fb11c1f5271bdbd512e227644aa8ce245
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-28 12:10:43 +0300

    create action that generates response according incoming values

--- app.rb
+++ app.rb
@@ -9,15 +9,6 @@ class App
     @handlers = { '/time': HandlerTimeFormat }
   end
 
-  def call(env)
-    @request = Rack::Request.new(env)
-    initialize_response_arguments
-    process_call
-    Rack::Response.new(@body, @status, @headers).finish
-  end
-
-  private
-
   ##
   # main method of incoming call handling
   # It Prepare 3 states:
@@ -25,24 +16,20 @@ class App
   #   2 - if handler has errors (400 and errors message in body)
   #   3 - normal state when request handled successfully
   #  All addition logic will be described inside each handler according it business logic
-  def process_call
-    unless @handlers.key?(@request.path.to_sym)
-      @status = 404
-      return
-    end
+  def call(env)
+    request = Rack::Request.new(env)
+    return response(404) unless @handlers.key?(request.path.to_sym)
 
-    handler = @handlers[@request.path.to_sym].new(@request.params['format'])
-    @status = 400 if handler.errors?
-    @body = handler.errors? ? handler.errors : handler.prepare_result
+    handler = @handlers[request.path.to_sym].new(request.params['format'])
+    return response(400, handler.errors) if handler.errors?
+
+    response(body: handler.prepare_result)
+
   end
 
-  ##
-  # Initialization executes in separate action
-  # It's need to clean data for each request call
-  def initialize_response_arguments
-    @body = nil
-    @status = 200
-    @headers = { 'Content-Type' => 'text/plain' }
-  end
+  private
 
+  def response(status = 200, body = nil, headers = { 'Content-Type' => 'text/plain' })
+    Rack::Response.new(body, status, headers).finish
+  end
 end
